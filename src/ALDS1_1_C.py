3bdbef5ee320e71048253d4d4dcd4859ca55cd4e
zonanjaro86 <zonanjaro86@gmail.com>
2020-08-03 16:17:41 +0900

    Update: AC ALDS1_1_C

--- src/ALDS1_1_C.py
+++ src/ALDS1_1_C.py
@@ -1,8 +1,11 @@
+import math
+
 def isPrime(k):
     # 2以外の偶数は素数ではない
     if k % 2 == 0 and k != 2:
         return False
-    for divisor in range(2, k//2):
+    # 平方根までの範囲で約数をもつかどうか 
+    for divisor in range(2, math.floor(math.sqrt(k))+1):
         if k % divisor == 0:
             return False
     return True
